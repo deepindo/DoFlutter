b8459875bcb497be4529614c6590ab4a0bd505a4
deepindo <732872042@qq.com>
2023-02-21 20:50:03 +0800

    集成了几个pub，其中轮播器pagecontrol不显示当前对应index的颜色，几年没有更新了

--- lib/pages/tabBar/home_page.dart
+++ lib/pages/tabBar/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:doflutter/components/do_dialog.dart';
 import 'package:doflutter/widget/page_header.dart';
 import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:banner_carousel/banner_carousel.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +13,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final List<String> _bannerList = [
+    "https://www.itying.com/images/flutter/1.png",
+    "https://www.itying.com/images/flutter/2.png",
+    "https://www.itying.com/images/flutter/3.png",
+    "https://www.itying.com/images/flutter/4.png",
+    "https://www.itying.com/images/flutter/5.png"
+  ];
+
+  final List<BannerModel> _listBanners = [
+    BannerModel(
+        imagePath: "https://www.itying.com/images/flutter/1.png", id: "1"),
+    BannerModel(
+        imagePath: "https://www.itying.com/images/flutter/2.png", id: "2"),
+    BannerModel(
+        imagePath: "https://www.itying.com/images/flutter/3.png", id: "3"),
+    BannerModel(
+        imagePath: "https://www.itying.com/images/flutter/4.png", id: "4"),
+    BannerModel(
+        imagePath: "https://www.itying.com/images/flutter/5.png", id: "5"),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,7 +62,53 @@ class _HomePageState extends State<HomePage> {
           },
         ),
       ),
-      body: TextButton(onPressed: () {}, child: const Text("测试")),
+      body: Container(
+        color: Colors.blue,
+        width: double.infinity,
+        height: 200,
+        child: _flutterSwiper(),
+      ),
+    );
+  }
+
+  //不能滚动
+  Widget _bannerCarousel() {
+    return BannerCarousel(
+      banners: _listBanners,
+      customizedIndicators: const IndicatorModel.animation(
+          width: 20, height: 5, spaceBetween: 5, widthAnimation: 50),
+      height: 200,
+      activeColor: Colors.amberAccent,
+      disableColor: Colors.white,
+      animation: true,
+      // borderRadius: 10,
+      width: double.infinity,
+      indicatorBottom: false,
+    );
+  }
+
+  Widget _flutterSwiper() {
+    return Swiper(
+      itemCount: _bannerList.length,
+      index: 0,
+      autoplay: true,
+      autoplayDelay: 6000,
+      // containerWidth: double.infinity,
+      // containerHeight: 200,
+      pagination: const SwiperPagination(
+        alignment: Alignment.bottomCenter,
+        builder: SwiperPagination.dots,
+        // builder: (BuildContext context, SwiperPluginConfig config) {},
+      ),
+      control: const SwiperControl(size: 15),
+      itemBuilder: (context, index) {
+        return Image.network(
+          _bannerList[index],
+          fit: BoxFit.cover,
+        );
+      },
+      onTap: (index) {},
+      onIndexChanged: (value) {},
     );
   }
 }
